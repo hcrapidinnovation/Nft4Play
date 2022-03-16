40c1c920014e5e81e8d5e1c9779f6e35a01e378e
Jeetanshu <jeetanshu18srivastava@gmail.com>
2022-03-16 16:18:30 +0530

    Comment Cow Code

--- src/cow-card/cow-card.controller.ts
+++ src/cow-card/cow-card.controller.ts
@@ -20,42 +20,42 @@ import { CowCardService as CardService } from './cow-card.service'
 export class CowCardController {
   constructor(private readonly metadataNFTService: CardService) {}
 
-  @Get('metadata/:nftId.json')
-  async getOpenSeaMetadata(
-    @Param('nftId', ParseIntPipe) nftId: number,
-  ): Promise<IOpenSeaMetadata | unknown> {
-    return this.metadataNFTService.getOpenSeaMetadata(nftId)
-  }
+  // @Get('metadata/:nftId.json')
+  // async getOpenSeaMetadata(
+  //   @Param('nftId', ParseIntPipe) nftId: number,
+  // ): Promise<IOpenSeaMetadata | unknown> {
+  //   return this.metadataNFTService.getOpenSeaMetadata(nftId)
+  // }
 
-  @Get('metadata/internal/:nftId.json')
-  async getOpenSeaMetadataInternal(
-    @Param('nftId', ParseIntPipe) nftId: number,
-  ): Promise<IOpenSeaMetadata | unknown> {
-    return this.metadataNFTService.getOpenSeaMetadataInternal(nftId)
-  }
+  // @Get('metadata/internal/:nftId.json')
+  // async getOpenSeaMetadataInternal(
+  //   @Param('nftId', ParseIntPipe) nftId: number,
+  // ): Promise<IOpenSeaMetadata | unknown> {
+  //   return this.metadataNFTService.getOpenSeaMetadataInternal(nftId)
+  // }
 
-  @Post('/:secret')
-  async createMetadataNft(
-    @Param('secret') secret: string,
-    @Body() createDto: CreateMetadataNFTDto,
-  ): Promise<CardMetadataNFT> {
-    return this.metadataNFTService.createMetadataNFT(secret, createDto)
-  }
+  // @Post('/:secret')
+  // async createMetadataNft(
+  //   @Param('secret') secret: string,
+  //   @Body() createDto: CreateMetadataNFTDto,
+  // ): Promise<CardMetadataNFT> {
+  //   return this.metadataNFTService.createMetadataNFT(secret, createDto)
+  // }
 
-  @Post('/upload/csv/:secret')
-  @UseInterceptors(FileInterceptor('csv', multerStoragePath('temp')))
-  async bulkCreateMetadataNFT(
-    @Param('secret') secret: string,
-    @UploadedFile() file: Express.Multer.File,
-  ): Promise<any> {
-    return this.metadataNFTService.bulkCreateMetadataNFT(secret, file)
-  }
+  // @Post('/upload/csv/:secret')
+  // @UseInterceptors(FileInterceptor('csv', multerStoragePath('temp')))
+  // async bulkCreateMetadataNFT(
+  //   @Param('secret') secret: string,
+  //   @UploadedFile() file: Express.Multer.File,
+  // ): Promise<any> {
+  //   return this.metadataNFTService.bulkCreateMetadataNFT(secret, file)
+  // }
 
-  @Patch('/:secret')
-  async updateMetadataNft(
-    @Param('secret') secret: string,
-    @Body() updateDto: UpdateMetadataNFTDto,
-  ): Promise<CardMetadataNFT> {
-    return this.metadataNFTService.updateMetadataNft(secret, updateDto)
-  }
+  // @Patch('/:secret')
+  // async updateMetadataNft(
+  //   @Param('secret') secret: string,
+  //   @Body() updateDto: UpdateMetadataNFTDto,
+  // ): Promise<CardMetadataNFT> {
+  //   return this.metadataNFTService.updateMetadataNft(secret, updateDto)
+  // }
 }
--- src/cow-medal/cow-medal.controller.ts
+++ src/cow-medal/cow-medal.controller.ts
@@ -23,42 +23,42 @@ import { CowMedalService } from './cow-medal.service'
 export class CowMedalController {
   constructor(private readonly metadataNFTService: CowMedalService) {}
 
-  @Get('metadata/:batchId.json')
-  async getOpenSeaMetadata(
-    @Param('batchId', ParseIntPipe) batchId: number,
-  ): Promise<IOpenSeaMetadata | unknown> {
-    return this.metadataNFTService.getOpenSeaMetadata(batchId)
-  }
+  // @Get('metadata/:batchId.json')
+  // async getOpenSeaMetadata(
+  //   @Param('batchId', ParseIntPipe) batchId: number,
+  // ): Promise<IOpenSeaMetadata | unknown> {
+  //   return this.metadataNFTService.getOpenSeaMetadata(batchId)
+  // }
 
-  @Get('metadata/internal/:batchId.json')
-  async getOpenSeaMetadataInternal(
-    @Param('batchId', ParseIntPipe) batchId: number,
-  ): Promise<IOpenSeaMetadata | unknown> {
-    return this.metadataNFTService.getOpenSeaMetadataInternal(batchId)
-  }
+  // @Get('metadata/internal/:batchId.json')
+  // async getOpenSeaMetadataInternal(
+  //   @Param('batchId', ParseIntPipe) batchId: number,
+  // ): Promise<IOpenSeaMetadata | unknown> {
+  //   return this.metadataNFTService.getOpenSeaMetadataInternal(batchId)
+  // }
 
-  @Post('/:secret')
-  async createMetadataNft(
-    @Param('secret') secret: string,
-    @Body() createDto: CreateMetadataNFTDto,
-  ): Promise<MedalMetadataNFT> {
-    return this.metadataNFTService.createMetadataNFT(secret, createDto)
-  }
+  // @Post('/:secret')
+  // async createMetadataNft(
+  //   @Param('secret') secret: string,
+  //   @Body() createDto: CreateMetadataNFTDto,
+  // ): Promise<MedalMetadataNFT> {
+  //   return this.metadataNFTService.createMetadataNFT(secret, createDto)
+  // }
 
-  @Post('/upload/csv/:secret')
-  @UseInterceptors(FileInterceptor('csv', multerStoragePath('temp')))
-  async bulkCreateMetadataNFT(
-    @Param('secret') secret: string,
-    @UploadedFile() file: Express.Multer.File,
-  ): Promise<any> {
-    return this.metadataNFTService.bulkCreateMetadataNFT(secret, file)
-  }
+  // @Post('/upload/csv/:secret')
+  // @UseInterceptors(FileInterceptor('csv', multerStoragePath('temp')))
+  // async bulkCreateMetadataNFT(
+  //   @Param('secret') secret: string,
+  //   @UploadedFile() file: Express.Multer.File,
+  // ): Promise<any> {
+  //   return this.metadataNFTService.bulkCreateMetadataNFT(secret, file)
+  // }
 
-  @Patch('/:secret')
-  async updateMetadataNft(
-    @Param('secret') secret: string,
-    @Body() updateDto: UpdateMetadataNFTDto,
-  ): Promise<MedalMetadataNFT> {
-    return this.metadataNFTService.updateMetadataNft(secret, updateDto)
-  }
+  // @Patch('/:secret')
+  // async updateMetadataNft(
+  //   @Param('secret') secret: string,
+  //   @Body() updateDto: UpdateMetadataNFTDto,
+  // ): Promise<MedalMetadataNFT> {
+  //   return this.metadataNFTService.updateMetadataNft(secret, updateDto)
+  // }
 }
